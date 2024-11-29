ad7b290ba57c8245954bfb9bf96c338c34e43f69
Priyansh <Priyanshyadav1012@gmail.com>
2024-11-29 13:44:05 +0530

    Implementation of Cn Package and Shadcn Sheet

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const LoggedIn = { firstName: "Priyansh", lastName: "Yadav" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={LoggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image
+            src="/icons.logo.svg"
+            width={30}
+            height={30}
+            alt="menu icons"
+          />
+          <div className="">
+            <MobileNav user={LoggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
---
+++ components/MobileNav.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+
+const MobileNav = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
+
+  return (
+    <section className="h-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className="cursor-pointer items-center gap-1 px-4 flex"
+          >
+            <Image src="/icons/logo.svg" alt={"Logo"} width={34} height={34} />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Horizon
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+                  return (
+                    <Link
+                      key={item.label}
+                      href={item.route}
+                      className={cn("sidebar-link", {
+                        "bg-bank-gradient": isActive,
+                      })}
+                    >
+                      <div className="relative size-6 ">
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          fill
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+                      </div>
+                      {item.label}
+                    </Link>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
