1559d54267c9d39207b4eec6b0cf2c511e28b4ab
Priyansh <Priyanshyadav1012@gmail.com>
2025-01-04 23:22:45 +0530

    Footer Complete

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,17 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.action";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const LoggedIn = { firstName: "Priyansh", lastName: "Yadav" };
+  const LoggedIn = await getLoggedInUser();
+
+  if (!LoggedIn) redirect("/sign-in ");
 
   return (
     <main className="flex h-screen w-full font-inter">
